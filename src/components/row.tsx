972a4715602d1a7e3d64edced18649e58e89675e
Dines Poulsen <dines.poulsen@lastminute.com>
2020-01-29 08:49:40 +0100

    added the row view

---
+++ src/components/row.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import { CellView } from './cell';
+import { Cell } from '../domain/cell';
+
+interface GridRowProps {
+    cells: Cell[]
+}
+
+export function RowView(props: GridRowProps) {
+    const cells = props.cells.map((cell) => <CellView alive={cell.alive}></CellView>)
+    return (
+        <div className="row">
+            {cells}
+        </div>
+    )
+}
