f17bd8a49b3ee6e0b87aac8361aa6193cae88574
Dines Poulsen <dines.poulsen@lastminute.com>
2020-02-07 12:34:19 +0100

    added stop feature of the css animation

--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -1,24 +1,26 @@
 import * as React from "react";
 import { CellStatus } from "../domain/cellStatus";
+import { useContext } from 'react';
+import { ControlsContext } from './controlsContext';
 
 interface CellProps {
     status: CellStatus
 }
 
-function getClassName(status: CellStatus) {
+function getStatusClass(status: CellStatus) {
     switch (status) {
         case CellStatus.Alive:
-            return 'alive';
+            return '__alive';
         case CellStatus.Dying:
-            return 'dying';
+            return '__dying';
         default:
             return '';
     }
 }
 
 export function CellView({ status }: CellProps) {
-
+    const { isPlaying } = useContext(ControlsContext);
     return (
-        <div className={ `cell cell--${getClassName(status)}`}></div>
+        <div className={`cell cell${getStatusClass(status)} cell${isPlaying ? '' : '--paused'}`}></div>
     );
 }
